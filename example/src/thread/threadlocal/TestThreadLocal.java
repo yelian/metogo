5bfa0bdf7aaa3961852348b83a12e0e9da87091d
yel881111 <yel881111@hotmail.com>
2014-06-15 13:52:22 +0800

    add comment!

--- example/src/thread/threadlocal/TestThreadLocal.java
+++ example/src/thread/threadlocal/TestThreadLocal.java
@@ -7,17 +7,29 @@ public class TestThreadLocal {
 
 	private static Map<String, ThreadLocal<O>> map = new HashMap<String, ThreadLocal<O>>();
 	public static void main(String[] args) {
-		ThreadLocal<O> threadLocal = new ThreadLocal<O>();
-		/*ThreadLocal<O> threadLocal = new ThreadLocal<O>(){
+		
+		//方式1：
+		//ThreadLocal<O> threadLocal = new ThreadLocal<O>();
+		
+		/*
+		 * threadLocal设置线程变量的时候，只有通过ThreadLocal的匿名内部类覆盖initialValue方法。
+		 * 这样设置ThreadLocal变量值才可以被其余的线程得到。否则采用方式1的方法其余的线程无法获取线程局部变量值。
+		 * */
+		
+		//方式2：
+		ThreadLocal<O> threadLocal = new ThreadLocal<O>(){
 			
 			public O initialValue(){
 				return new O();
 			}
-		};*/
+		};
 		O o = new O();
 		threadLocal.set(o);
 		map.put("main", threadLocal);
 		System.out.println(o.toString());
+		
+		/*测试的时候需要注意，需要创建多个线程thread，只有通过thread才可以看到设置效果。
+		 * */
 		for(int x=0; x<4; x++){
 			new ThreadChangeValue(map).start();
 			try {
@@ -46,6 +58,7 @@ class ThreadChangeValue extends Thread{
 			System.out.println(o.toString());
 			o.set_int((int)Thread.currentThread().getId());
 			o.set_String(Thread.currentThread().getName());
+			threadLocal.set(o);
 			System.out.println(o.toString());
 		}
 		System.out.println("--------------------thread end----------------------\n");
